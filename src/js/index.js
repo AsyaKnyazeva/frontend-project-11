6cc15591f758ea520fabf37aa868f59359cba3c1
AsyaKnyazeva <knyazeva478@gmail.com>
2022-10-10 09:29:42 +0300

    step 5v1

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import '../scss/styles.scss';
 import i18n from 'i18next';
-import axios from 'axios';
 import view from './view.js';
+import _ from 'lodash';
 import validateURL from './validateURL.js';
 import resources from './locales/index.js';
+import parserRSS from './parser.js';
+import loadRSS from './RSS.js';
+import updateRSS from './update.js';
 
 export default () => {
   const i18nInstance = i18n.createInstance();
@@ -29,35 +32,6 @@ export default () => {
 
   const state = view(elements, i18nInstance);
 
-  const rssParser = (rssFeed) => {
-    const parsedXML = new DOMParser().parseFromString(rssFeed.data, 'application/xml');
-    const error = parsedXML.querySelector('parsererror');
-    if (error) {
-      state.errorType = 'rssParser';
-      return;
-    }
-
-    const feed = {
-      title: parsedXML.querySelector('channel title').textContent,
-      description: parsedXML.querySelector('channel description').textContent,
-    };
-
-    const posts = Array.from(parsedXML.querySelectorAll('item'))
-      .map((item) => {
-        const title = item.querySelector('title').textContent;
-        const link = item.querySelector('link').textContent;
-        const description = item.querySelector('description').textContent;
-        return { title, link, description };
-      });
-
-    return [feed, posts];
-  };
-
-  const loadRSS = (url) => {
-    const proxyURL = `https://allorigins.hexlet.app/raw?url=${url}`;
-    return axios.get(proxyURL);
-  };
-
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -77,9 +51,9 @@ export default () => {
       })
       .then((rss) => {
         state.form.processState = 'sent';
-        const [rssFeed, rssPosts] = rssParser(rss);
+        const [rssFeed, rssPosts] = parserRSS(rss);
         const feedID = _.uniqueId();
-        const feed = { ...rssFeed, id: feedID, };
+        const feed = { ...rssFeed, id: feedID, url, };
         const posts = rssPosts.map((post) => {
           return { ...post, id: _.uniqueId(), feedID };
         });
@@ -91,4 +65,5 @@ export default () => {
         state.form.valid = false;
       });
   });
+  setTimeout(() => updateRSS(state), 1000);
 };
